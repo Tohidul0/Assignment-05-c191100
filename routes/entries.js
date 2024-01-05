eff86e2f72fffdceb9e92a36b54beedb152f24e6
Tohidul0 <tohidulalam967418@gmail.com>
2024-01-06 02:40:29 +0600

    PATCH & DELETE part working assignment5 done-----------------

--- routes/entries.js
+++ routes/entries.js
@@ -2,33 +2,33 @@ var express = require("express");
 const db = require("../database");
 var router = express.Router();
 
-// router.get("/", async function (req, res) {
-//     // fetch data from postgres
-//     const result = await db.query("SELECT * FROM entries;");
+router.get("/", async function (req, res) {
+    // fetch data from postgres
+    const result = await db.query("SELECT * FROM entries;");
 
-//     // send the data as response
-//     res.send(result.rows);
-// });
+    // send the data as response
+    res.send(result.rows);
+});
 
 router.post("/", async function (req, res) {
     // read data from client
     const { title, value, type } = req.body;
 
-    const errors = [];
+    const errors1 = [];
     if (title.length < 5) {
-        errors.push("Title is too short");
+        errors1.push("Title is too short");
     }
     if (value < 0) {
-        errors.push("Value must be positive");
+        errors1.push("Value must be positive");
     }
     if (!["income", "expense"].includes(type)) {
-        errors.push("Invalid type - please use expense or income");
+        errors1.push("Invalid type - please use expense or income");
     }
 
-    if (errors.length > 0) {
+    if (errors1.length > 0) {
         return res.status(400).send({
             errorType: "VALIDATION_ERROR",
-            errors,
+            errors1,
         });
     }
 
@@ -62,7 +62,62 @@ router.get("/:id", async function (req, res){
     // send the entry as response
     res.send(result.rows[0]);
 });
-// PATCH /:id - update single entry
-// DELTE /:id - delete single entry
+// GET part end here-----------------------------------------------------
+
+
+
+
+// PATCH /:id - update single entry-------------task2--------------
+router.patch("/:id", async function (req, res) {
+    const entryId = req.params.id;
+    console.log(entryId);
+    const { title, value, type } = req.body;
+
+    const errors = [];
+    if (title && title.length < 5) {
+        errors.push("Title is too short");
+    }
+    if (value && value < 0) {
+        errors.push("Value must be positive");
+    }
+    if (type && !["income", "expense"].includes(type)) {
+        errors.push("Invalid type - please use expense or income");
+    }
+
+    if (errors.length > 0) {
+        return res.status(400).send({
+            errorType: "VALIDATION_ERROR",
+            errors,
+        });
+    }
+
+    // update data in the database
+    const result = await db.query(
+        `UPDATE entries SET title = COALESCE($1, title), value = COALESCE($2, value), type = COALESCE($3, type) WHERE id = $4 RETURNING *;`,
+        [title, value, type, entryId]
+    );
+    
+
+    if (result.rows.length === 0) {
+        return res.status(404).send({
+            errorType: "NOT_FOUND_ERROR",
+            message: "Entry not found",
+        });
+    }
+
+    // send the updated entry as response
+    res.send(result.rows[0]);
+});
+// PATCH part end here-----------------------------
+
+
+// DELTE /:id - delete single entry------------------task3---------
+
+router.delete("/:id",async function(req,res){
+    const entryId= req.params.id;
+    const result = await db.query(`DELETE FROM entries WHERE id=$1;`,[entryId]);
+    res.send(result.rows[0]);
+});
+// --------------delete part end here----------------------------
 
 module.exports = router;
